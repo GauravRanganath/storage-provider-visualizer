e3797b5bebd68b659886596481cc3ba3e223180d
Gaurav Ranganath <gauravranganath@Gauravs-MBP.ht.home>
2023-01-30 21:08:15 -0500

    test get request on fleek

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+const getContentByCid = () => {
+  fetch(
+    "https://api.estuary.tech/public/by-cid/QmVrrF7DTnbqKvWR7P7ihJKp4N5fKmBX29m5CHbW9WLep9"
+  ).then((res) => console.log(res.data));
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={getContentByCid}>Get Content By CID</button>
     </div>
   );
 }
