b33191c17d3a2ef38ff5d1b53313675b88b78cc7
Gaurav Ranganath <gauravranganath@Gauravs-MBP.ht.home>
2023-01-31 01:41:33 -0500

    add globe

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Globe from "react-globe.gl";
 
 function getLocations(ipAddress) {
   return new Promise((resolve, reject) => {
@@ -64,9 +65,29 @@ const getContentByCid = () => {
 };
 
 function App() {
+  const N = 20;
+  const arcsData = [...Array(N).keys()].map(() => ({
+    startLat: (Math.random() - 0.5) * 180,
+    startLng: (Math.random() - 0.5) * 360,
+    endLat: (Math.random() - 0.5) * 180,
+    endLng: (Math.random() - 0.5) * 360,
+    color: [
+      ["red", "white", "blue", "green"][Math.round(Math.random() * 3)],
+      ["red", "white", "blue", "green"][Math.round(Math.random() * 3)],
+    ],
+  }));
+
   return (
     <div>
       <button onClick={getContentByCid}>Get Content By CID</button>
+      <Globe
+        globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+        arcsData={arcsData}
+        arcColor={"color"}
+        arcDashLength={() => Math.random()}
+        arcDashGap={() => Math.random()}
+        arcDashAnimateTime={() => Math.random() * 4000 + 500}
+      ></Globe>
     </div>
   );
 }
