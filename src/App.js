cc8e216d1ad943a3bcf45a8f27631bd6d470bffb
Gaurav Ranganath <gauravranganath@Gauravs-MBP.ht.home>
2023-01-31 01:55:52 -0500

    working arc builder

--- src/App.js
+++ src/App.js
@@ -1,70 +1,95 @@
 import "./App.css";
 import Globe from "react-globe.gl";
-
-function getLocations(ipAddress) {
-  return new Promise((resolve, reject) => {
-    fetch(`https://ipapi.co/${ipAddress}/json/`).then((res) =>
-      res.json().then((data) => {
-        resolve(data);
-      })
-    );
-  });
-}
-
-function getIpAddresses(minerId) {
-  return new Promise((resolve, reject) => {
-    fetch(`https://api.estuary.tech/public/miners/stats/${minerId}`)
-      .then((res) => res.json())
-      .then((data) => {
-        let ipAddresses = [];
-        let addresses = data.chainInfo.addresses;
-        addresses.forEach((address) => {
-          ipAddresses.push(address.split("/")[2]);
-        });
-        resolve(ipAddresses);
-      });
-  });
-}
-
-const getContentByCid = () => {
-  fetch(
-    "https://api.estuary.tech/public/by-cid/QmVrrF7DTnbqKvWR7P7ihJKp4N5fKmBX29m5CHbW9WLep9"
-  )
-    .then(async (res) => {
-      let data = await res.json();
-      return data;
-    })
-    .then((data) => {
-      let minerIds = [];
-      data.forEach((e) => {
-        e.deals.forEach((miners) => {
-          minerIds.push(miners.miner);
-        });
-      });
-      return minerIds;
-    })
-    .then((minerIds) => {
-      let ipAddresses = [];
-      minerIds.forEach((minerId) => {
-        ipAddresses.push(getIpAddresses(minerId));
-      });
-      Promise.all(ipAddresses)
-        .then((allIpAddresses) => {
-          return allIpAddresses.flat();
-        })
-        .then((ipAddresses) => {
-          let locationData = [];
-          ipAddresses.forEach((ipAddress) => {
-            locationData.push(getLocations(ipAddress));
-          });
-          Promise.all(locationData).then((allLocationData) => {
-            console.log(allLocationData);
-          });
-        });
-    });
-};
+import { useState } from "react";
 
 function App() {
+  const [arcs, setArcs] = useState([]);
+
+  function getLocations(ipAddress) {
+    return new Promise((resolve, reject) => {
+      fetch(`https://ipapi.co/${ipAddress}/json/`).then((res) =>
+        res.json().then((data) => {
+          resolve(data);
+        })
+      );
+    });
+  }
+
+  function getIpAddresses(minerId) {
+    return new Promise((resolve, reject) => {
+      fetch(`https://api.estuary.tech/public/miners/stats/${minerId}`)
+        .then((res) => res.json())
+        .then((data) => {
+          let ipAddresses = [];
+          let addresses = data.chainInfo.addresses;
+          addresses.forEach((address) => {
+            ipAddresses.push(address.split("/")[2]);
+          });
+          resolve(ipAddresses);
+        });
+    });
+  }
+
+  const getContentByCid = () => {
+    fetch(
+      "https://api.estuary.tech/public/by-cid/QmVrrF7DTnbqKvWR7P7ihJKp4N5fKmBX29m5CHbW9WLep9"
+    )
+      .then(async (res) => {
+        let data = await res.json();
+        return data;
+      })
+      .then((data) => {
+        let minerIds = [];
+        data.forEach((e) => {
+          e.deals.forEach((miners) => {
+            minerIds.push(miners.miner);
+          });
+        });
+        return minerIds;
+      })
+      .then((minerIds) => {
+        let ipAddresses = [];
+        minerIds.forEach((minerId) => {
+          ipAddresses.push(getIpAddresses(minerId));
+        });
+        Promise.all(ipAddresses)
+          .then((allIpAddresses) => {
+            return allIpAddresses.flat();
+          })
+          .then((ipAddresses) => {
+            let locationData = [];
+            ipAddresses.forEach((ipAddress) => {
+              locationData.push(getLocations(ipAddress));
+            });
+            Promise.all(locationData).then((allLocationData) => {
+              console.log(allLocationData);
+              let arcBuilder = [];
+              let myLat = 43.6534817
+              let myLong = -79.3839347
+
+              allLocationData.forEach((location) => {
+                arcBuilder.push({
+                  startLat: myLat,
+                  startLng: myLong,
+                  endLat: location.latitude,
+                  endLng: location.longitude,
+                  color: [
+                    ["red", "white", "blue", "green"][
+                      Math.round(Math.random() * 3)
+                    ],
+                    ["red", "white", "blue", "green"][
+                      Math.round(Math.random() * 3)
+                    ],
+                  ],
+                });
+              });
+
+              setArcs(arcBuilder)
+            });
+          });
+      });
+  };
+
   const N = 20;
   const arcsData = [...Array(N).keys()].map(() => ({
     startLat: (Math.random() - 0.5) * 180,
@@ -82,7 +107,7 @@ function App() {
       <button onClick={getContentByCid}>Get Content By CID</button>
       <Globe
         globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
-        arcsData={arcsData}
+        arcsData={arcs}
         arcColor={"color"}
         arcDashLength={() => Math.random()}
         arcDashGap={() => Math.random()}
