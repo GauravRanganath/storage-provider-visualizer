ab6144015a897d3d2f41c77cfafb5c799c11b002
Gaurav Ranganath <gauravranganath@Gauravs-MBP.ht.home>
2023-02-01 18:43:24 -0500

    add styling and functionality

--- src/App.js
+++ src/App.js
@@ -7,10 +7,17 @@ import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 
 function App() {
+  const [cid, setCid] = useState(
+    "QmVrrF7DTnbqKvWR7P7ihJKp4N5fKmBX29m5CHbW9WLep9"
+  );
   const [points, setPoints] = useState([]);
+  const [pointHover, setPointHover] = useState(false);
   const [arcs, setArcs] = useState([]);
   const [rings, setRings] = useState([]);
   const [myLocation, setMyLocation] = useState({});
+  const [allLocations, setAllLocations] = useState({});
+  const [clickedLocation, setClickedLocation] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     fetch(`https://ipapi.co/json/`).then((res) =>
@@ -46,10 +53,27 @@ function App() {
     });
   }
 
+  const findLocation = (coords) => {
+    setPointHover(true);
+
+    allLocations.forEach((elem) => {
+      if (elem.latitude === coords.lat && elem.longitude === coords.lng) {
+        setClickedLocation(elem);
+      }
+    });
+  };
+
+  const handleCidChange = (event) => {
+    setCid(event.target.value);
+  };
+
+  const populateRandomCid = () => {
+    setCid(hardCids[Math.floor(Math.random() * hardCids.length)]);
+  };
+
   const getContentByCid = () => {
-    fetch(
-      "https://api.estuary.tech/public/by-cid/QmVrrF7DTnbqKvWR7P7ihJKp4N5fKmBX29m5CHbW9WLep9"
-    )
+    setIsLoading(true);
+    fetch(`https://api.estuary.tech/public/by-cid/${cid}`)
       .then(async (res) => {
         let data = await res.json();
         return data;
@@ -65,11 +89,13 @@ function App() {
       })
       .then((minerIds) => {
         let ipAddresses = [];
+
         minerIds.forEach((minerId) => {
           ipAddresses.push(getIpAddresses(minerId));
         });
         Promise.all(ipAddresses)
           .then((allIpAddresses) => {
+            setIsLoading(false);
             return allIpAddresses.flat();
           })
           .then((ipAddresses) => {
@@ -79,6 +105,8 @@ function App() {
             });
             Promise.all(locationData).then((allLocationData) => {
               console.log(allLocationData);
+
+              let locationsBuilder = [];
               let pointsBuilder = [];
               let arcBuilder = [];
               let ringBuilder = [];
@@ -94,27 +122,31 @@ function App() {
                   startLng: location.longitude,
                   endLat: myLocation.latitude,
                   endLng: myLocation.longitude,
+                  color: "#39FF14",
                 });
 
                 ringBuilder.push({
                   lat: location.latitude,
                   lng: location.longitude,
+                  color: "#39FF14",
                 });
               });
 
               pointsBuilder.push({
                 lat: myLocation.latitude,
                 lng: myLocation.longitude,
+                color: "#ffffff",
               });
 
               ringBuilder.push({
                 lat: myLocation.latitude,
                 lng: myLocation.longitude,
-                maxR: 5,
-                propagationSpeed: 1,
-                repeatPeriod: 100,
+                color: "#ffffff",
               });
 
+              allLocationData.push(myLocation);
+
+              setAllLocations(allLocationData);
               setPoints(pointsBuilder);
               setArcs(arcBuilder);
               setRings(ringBuilder);
@@ -123,25 +155,101 @@ function App() {
       });
   };
 
+  const retrieveInfo = () => {
+    getContentByCid();
+  };
+
+  let hardCids = [
+    "QmbWqxBEKC3P8tqsKc98xmWNzrzDtRLMiMPL8wBuTGsMnR",
+    "QmVrrF7DTnbqKvWR7P7ihJKp4N5fKmBX29m5CHbW9WLep9",
+  ];
+
   return (
     <div className="parent">
       <div className="child panel">
         <h1 style={{ fontSize: "64px" }}>DEOXYS</h1>
-        <p>See where your IPFS data is stored using Estuary's public API</p>
-        <InputGroup style={{ borderColor: "#39ff14" }}>
-          <Form.Control placeholder="Enter CID" />
-          <Button
-            onClick={getContentByCid}
-            variant="outline-secondary"
-            style={{ color: "#39ff14", borderColor: "#39ff14" }}
-          >
-            GO
-          </Button>
-        </InputGroup>
+        <p>See where IPFS data is stored using Estuary's public API</p>
+
+        {isLoading === false && (
+          <div>
+            <input
+              value={cid}
+              onChange={handleCidChange}
+              placeholder="Enter CID"
+            />
+            <br />
+            <br />
+            <button
+              className="btn"
+              onClick={retrieveInfo}
+              style={{ marginRight: "25px" }}
+            >
+              VIEW INFO
+            </button>
+            <button className="btn" onClick={populateRandomCid}>
+              USE RANDOM CID
+            </button>
+          </div>
+        )}
+
+        {isLoading === true && (
+          <p className="loadingBanner">RETRIEVING . . .</p>
+        )}
+        {pointHover === false && (
+          <>
+            <br />
+            <h6>CLICK ON A STORAGE PROVIDER MARKER TO VIEW INFO</h6>
+          </>
+        )}
+        {pointHover === true && (
+          <div className="storageProviderInfo">            
+            <h6>GEOLOCATION</h6>
+            <div class="row">
+              <div class="column">
+                <p>City: {clickedLocation.city}</p>
+              </div>
+              <div class="column">
+                <p>Region: {clickedLocation.region}</p>
+              </div>
+            </div>
+            <div class="row">
+              <div class="column">
+                <p>Country: {clickedLocation.country_name}</p>
+              </div>
+              <div class="column">
+                <p>Timezone: {clickedLocation.timezone}</p>
+              </div>
+            </div>
+            <div class="row">
+              <div class="column">
+                <p>Latitude: {clickedLocation.latitude}</p>
+              </div>
+              <div class="column">
+                <p>Longitude: {clickedLocation.longitude}</p>
+              </div>
+            </div>
+            <h6>NETWORK DETAILS</h6>
+            <div class="row">
+              <div class="column">
+              <p>Organization: {clickedLocation.org}</p>
+              </div>
+            </div>
+            <div class="row">
+              <div class="column">
+                <p>IP: {clickedLocation.ip}</p>
+              </div>
+              <div class="column">
+                <p>Network: {clickedLocation.network}</p>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
+
       <div className="child">
         <Globe
           width={window.innerWidth / 2}
+          //atmosphereColor={"#39FF14"}
           arcsData={arcs}
           arcColor={() => "#39FF14"}
           arcDashLength={0.05}
@@ -151,7 +259,11 @@ function App() {
           ringColor={() => "#39FF14"}
           pointAltitude={0.025}
           pointsData={points}
+          pointRadius={1}
           pointColor={() => "#39FF14"}
+          onPointClick={(coords) => {
+            findLocation(coords);
+          }}
           globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
         />
       </div>
